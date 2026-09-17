a77c92f0b7d57f48f3d3768e93db880b396847b0
yygg <66yygg99@gmail.com>
2026-09-17 18:21:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'future-dictionary-v11-16';
+const CACHE = 'future-dictionary-v11-17';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
