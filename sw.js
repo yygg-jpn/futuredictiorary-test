5ad6671e20eccc0f824d11496b5b41e520de8d77
yygg <66yygg99@gmail.com>
2026-09-17 17:45:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'future-dictionary-v11-16';
+const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy));
+    return resp;
+  }).catch(() => caches.match('./index.html'))));
+});
